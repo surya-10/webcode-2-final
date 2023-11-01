13bc5ee8191cf2df7fa0859bbee878be04ad8e47
surya-10 <sp659151@gmail.com>
2023-11-01 15:31:59 +0530

    done

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { client } from "./db.js";
+// import { client } from "./db.js";
 dotenv.config()
 import { searchRoute } from "./controller/controller.js";
-import {fridgeData, watches, earPhones, refreshAllLaptops, refreshAllMobiles } from "./controller/getAllDatas.js";
+// import {fridgeData, watches, earPhones, refreshAllLaptops, refreshAllMobiles } from "./controller/getAllDatas.js";
 let app = express();
 app.use(cors())
 app.use(express.json());
